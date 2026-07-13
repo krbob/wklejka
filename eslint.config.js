9989f822af7f0edb3228d24cf0d6fa20e6868a60
Krzysztof Bobiński <krzysztof@bobinski.net>
2026-07-13 17:43:20 +0200

    chore: add project quality gates

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: [
+      'coverage/**',
+      'data/**',
+      'node_modules/**',
+      'public/icons/**',
+    ],
+  },
+  {
+    files: [
+      '*.js',
+      'lib/**/*.js',
+      'scripts/**/*.js',
+      'test/**/*.js',
+    ],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+    rules: {
+      'no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        caughtErrors: 'none',
+      }],
+    },
+  },
+  {
+    files: ['public/**/*.js'],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: {
+        ...globals.browser,
+        ...globals.worker,
+        module: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        caughtErrors: 'none',
+      }],
+    },
+  },
+];
